b928b44befb64265ce89b9af5a883e6f2ea9865c
jdm79 <james.malvern@gmail.com>
2018-08-13 15:49:34 +0100

    refactored top_up test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,9 +6,14 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
-  it 'tops up the balance' do
-    subject.top_up(10)
-    expect(subject.balance).to eq 10
+  describe '#top_up' do
+
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it "can top up the balance" do
+      expect{ subject.top_up 10 }.to change{ subject.balance }.by 10
+    end
+
   end
 
 end
