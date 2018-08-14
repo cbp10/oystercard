518a16e53ca1b5082ed3ff02188c356b9dee6eac
James Sutherland <jrsutherland78@googlemail.com>
2018-08-14 09:52:08 +0100

    Add #touch_in and #touch_out with tests

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,7 @@
 class Oystercard
 
   BALANCE_LIMIT = 90
+  MINIMUM_BALANCE = 1
 
   attr_reader :balance
   attr_accessor :in_journey
@@ -20,6 +21,7 @@ class Oystercard
   end
 
   def touch_in
+    fail 'Insufficient funds to travel' if @balance < MINIMUM_BALANCE
     @in_journey = true
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -27,17 +27,9 @@ describe Oystercard do
       expect{ subject.deduct 10 }.to change{ subject.balance }.by -10
     end
 
-    # it "raises an error when the balance limit is exceeded" do
-    #   expect{ subject.top_up(91) }.to raise_error "Balance limit of £#{Oystercard::BALANCE_LIMIT} exceeded"
-    # end
-
   end
 
-  describe '#in_journey?' do
-
-    it 'card not on a journey' do
-      expect(subject).not_to be_in_journey?
-    end
+  describe '#in_journey' do
 
     it 'returns a boolean' do
     expect(subject.in_journey).to be(true).or be(false)
@@ -47,9 +39,23 @@ describe Oystercard do
 
   describe '#touch_in' do
 
-    it 'can touch in' do
+    it 'can touch in when balance is more than minimum required' do
+      subject.top_up(10)
       subject.touch_in
-      expect(subject).to be_in_journey
+      expect(subject.in_journey).to eq(true)
+    end
+
+    it 'will not touch in if balance is below minimum balance to travel' do
+      expect{ subject.touch_in }.to raise_error 'Insufficient funds to travel'
+    end
+
+  end
+
+  describe '#touch_out' do
+
+    it 'can touch out' do
+      subject.touch_out
+      expect(subject.in_journey).to eq(false)
     end
 
   end
